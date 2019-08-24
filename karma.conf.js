74907c866aa6b1553b5ed5000cb273bd8c31990d
Pawel <jarrodek@gmail.com>
2019-08-24 00:03:19 -0700

    fix: fixing karma config for Edge

--- karma.conf.js
+++ karma.conf.js
@@ -6,6 +6,12 @@ module.exports = (config) => {
   config.set(
     merge(createDefaultConfig(config), {
       files: [
+        {
+          pattern: require.resolve('chai/chai.js')
+        },
+        {
+          pattern: require.resolve('axe-core/axe.min.js')
+        },
         // runs all files ending with .test in the test folder,
         // can be overwritten by passing a --grep flag. examples:
         //
